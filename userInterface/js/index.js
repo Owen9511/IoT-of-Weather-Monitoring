c8588716deb68a55683636ba6b46a823fee0e4ea
MINGNUO <zhangkeyuwork@163.com>
2018-07-22 17:09:57 +0100

    seperate all js and css code from html

--- userInterface/js/index.js
+++ userInterface/js/index.js
@@ -2,6 +2,8 @@ function main() {
   var $select = $('#source'),
     $variable = $('.variable'),
     $time = $('#time'),
+    $txt = $('.txt').eq(0),
+    $dmy = $('.dmy').eq(0),
     source = $select.val();
 
   function renewData() {
@@ -33,6 +35,33 @@ function main() {
     //immediately execute renewData() and execute it every 10000ms
     renewData();
     setTimeout(cyclicRenew, 10000);
+  })();
+
+  (function renewTime() {
+    //format and renew time display every 1000ms
+    var today = new Date(),
+      h = today.getHours(),
+      m = today.getMinutes(),
+      s = today.getSeconds(),
+      Y = today.getYear() + 1900,
+      M = today.getMonth(),
+      D = today.getDay(),
+      date = today.getDate(),
+      dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"),
+      montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+    checkTime = function(i) {
+      if (i < 10) {
+        i = "0" + i
+      }; // add zero in front of numbers < 10
+      return i;
+    };
+
+    m = checkTime(m);
+    s = checkTime(s);
+    date = checkTime(date);
+    $txt.text(h + ":" + m + ":" + s);
+    $dmy.text("" + dayarray[D] + ", " + montharray[M] + " " + date + ", " + Y + "")
+    setTimeout(renewTime, 1000);
   })();
 }
 
